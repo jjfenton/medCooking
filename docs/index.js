a4753d721e97c8638ec450902759d49ea9c01d91
jjfenton <jjf1989@gmail.com>
2023-03-24 15:48:14 -0400

    working modal

--- docs/index.js
+++ docs/index.js
@@ -4,4 +4,10 @@ const menu = document.querySelector(".mobile-menu");
 btn.addEventListener("click", () =>{
     menu.classList.toggle("hidden");
     });
-       
+
+function toggleModal(modalID) {
+    document.getElementById(modalID).classList.toggle("hidden");
+    document.getElementById(modalID + "-backdrop").classList.toggle("hidden");
+    document.getElementById(modalID).classList.toggle("flex");
+    document.getElementById(modalID + "-backdrop").classList.toggle("flex");
+    }
